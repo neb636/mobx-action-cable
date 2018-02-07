df9bbe3e84e45a3514f4d6954dfae3918a57b13a
nblanchard <neb636@gmail.com>
2018-02-07 12:40:19 -0500

    Added provider to export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { applyMiddleware, createStore } from "./store";
-import { createStoreConnector } from "./create-store-connector";
+import { createStoreConnector } from './create-store-connector';
 import { observable, computed } from 'mobx';
+import { Provider } from 'mobx-react';
 
 export {
     createStoreConnector,
     applyMiddleware,
     createStore,
     observable,
-    computed
+    computed,
+    Provider
 };
